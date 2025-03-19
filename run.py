a45ba90af73022d6ba06b79bc58074ff10e4e141
CN_zzzwb <862324915@qq.com>
2025-03-19 17:36:29 +0800

    修改email推送接口

--- run.py
+++ run.py
@@ -77,7 +77,7 @@ def telegram_push(message):
         print(f"发送消息到Telegram失败: {response.text}")
 
 if push == "mail":
-    mail_push('https://zzzwb.us.kg/test')
+    mail_push('https://zzzwb.pp.ua/test')
 elif push == "telegram":
     telegram_push(content)
 else:
